a85ad8b10494fdf243a7576b965a4afa72805b3e
antoine <mr-maurice@wanadoo.fr>
2017-03-09 18:33:12 +0100

    half functional polling method

--- main.c
+++ main.c
@@ -7,13 +7,13 @@ uint8_t read_button_mute();
 uint8_t read_button_volp();
 uint8_t  read_button_volm();
  */
-uint8_t  read_btn();
-uint8_t test_for_press_only(void);
+uint8_t  read_btn(uint8_t);
+uint8_t test_for_press_only(uint8_t);
 
 //#define nop() __asm__ __volatile__ ("nop \n\t")
 
 //0x01: mute,0x02:volp, 0x04:volm
-volatile uint8_t curbtn=0x01;
+//volatile uint8_t curbtn=0x01;
 
 void setup() {
 	//LEDS
@@ -32,7 +32,7 @@ void setup() {
 	WDTCR |= (1<<WDTIE);//generate interrupt after each time out
 }
 
-uint8_t read_btn(){
+uint8_t read_btn(uint8_t  curbtn){
 	uint8_t ret=0x00;
 	/*
 	if (curbtn==0x01){
@@ -47,6 +47,18 @@ uint8_t read_btn(){
 		DDRB &=~_BV(PB2);//PB2 en entree
 		PORTB |=_BV(PB2);//pull-up actif
 		ret= ( (PINB & _BV(PB2)) == 0 );
+	} else if (curbtn==0x02){
+		DDRB |=_BV(PB2);//PB2 en sortie
+		PORTB &=~_BV(PB2);//PB2 a 0
+		DDRB &=~_BV(PB0);//PB0 en entree
+		PORTB |=_BV(PB0);//pull up sur PB0
+		ret= ( (PINB & _BV(PB0)) == 0 );
+	} else if (curbtn==0x04){
+		DDRB |=_BV(PB0);//PB0 en sortie
+		PORTB &=~_BV(PB0);//PB0 a 0
+		DDRB &=~_BV(PB4);//PB4 en entree
+		PORTB |=_BV(PB4);//pull up sur PB4
+		ret= ((PINB & (1<<PB4)) == 0);//lecture de PB0
 	}
 	return ret;
 }
@@ -76,11 +88,11 @@ uint8_t  read_button_volm(){
 }
  */
 
-uint8_t test_for_press_only(void){
+uint8_t test_for_press_only(uint8_t  curbtn){
 	static uint8_t button_history = 0;
 	uint8_t pressed = 0;
 	button_history = button_history << 1;
-	button_history |= read_btn();
+	button_history |= read_btn(curbtn);
 	if ((button_history & 0b11000111) == 0b00000111) {
 		pressed = 1;
 		button_history = 0b11111111;
@@ -99,10 +111,16 @@ int main() {
 }
 
 ISR (WDT_vect){
-	if (test_for_press_only()==1){
+	/*
+	if (test_for_press_only(0x01)==1){
 		PORTB ^= _BV(PB3);//flip led 1
 	}
-	PORTB ^= _BV(PB1);//flip led 1
+*/
+	if (test_for_press_only(0x04)==1){
+		PORTB ^= _BV(PB1);//flip led 1
+	}
+
+	//PORTB ^= _BV(PB1);//flip led 1
 
 	//cycles curbtn through 0x01, 0x02, 0x04
 	/*
@@ -111,12 +129,12 @@ ISR (WDT_vect){
 	}else{
 		curbtn=0x01;
 	}
-	*/
-/*
+	 */
+	/*
 	if ((curbtn | 0b00000011)==0b00000011) {
 		curbtn=curbtn<<1;
 	}else{
 		curbtn=0x01;
 	}
-*/
+	 */
 }
