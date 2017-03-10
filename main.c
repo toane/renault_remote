ab9f19fd16bde14721561c8c1b55c6ef0c6d32f4
antoine <mr-maurice@wanadoo.fr>
2017-03-10 10:36:35 +0100

    but with pointers

--- main.c
+++ main.c
@@ -2,18 +2,21 @@
 #include <util/delay.h>
 #include <avr/interrupt.h>
 #include <avr/sleep.h>
+//#define nop() __asm__ __volatile__ ("nop \n\t")
 /*
 uint8_t read_button_mute();
 uint8_t read_button_volp();
 uint8_t  read_button_volm();
  */
 uint8_t  read_btn(uint8_t);
-uint8_t debounce(uint8_t);
-
-//#define nop() __asm__ __volatile__ ("nop \n\t")
+uint8_t debounce(uint8_t  *button_history,uint8_t);
 
 //0x01: mute,0x02:volp, 0x04:volm
 //volatile uint8_t curbtn=0x01;
+
+uint8_t mute_history=0;
+uint8_t volp_history=0;
+uint8_t volm_history=0;
 
 void setup() {
 	//LEDS
@@ -58,43 +61,18 @@ uint8_t read_btn(uint8_t  curbtn){
 	return ret;
 }
 
-/*
-uint8_t read_button_mute(){
-	DDRB &=~_BV(PB2);//PB2 en entree
-	PORTB |=_BV(PB2);//pull-up actif
-	return ( (PINB & _BV(PB2)) == 0 );
-}
-
-uint8_t  read_button_volp(){
-	DDRB |=_BV(PB2);//PB2 en sortie
-	PORTB &=~_BV(PB2);//PB2 a 0
-	DDRB &=~_BV(PB0);//PB0 en entree
-	PORTB |=_BV(PB0);//pull up sur PB0
-	return ( (PINB & _BV(PB0)) == 0 );
-}
-
-uint8_t  read_button_volm(){
-	//VOLM
-	DDRB |=_BV(PB0);//PB0 en sortie
-	PORTB &=~_BV(PB0);//PB0 a 0
-	DDRB &=~_BV(PB4);//PB4 en entree
-	PORTB |=_BV(PB4);//pull up sur PB4
-	return ((PINB & (1<<PB4)) == 0);//lecture de PB0
-}
- */
-
 /***
  * curbtn: one of 0x01,0x02,0x04, matches mute, vol+,vol-
  * returns 1 if the button is considered pressed
  */
-uint8_t debounce(uint8_t  curbtn){
-	static uint8_t button_history = 0;
+uint8_t debounce(uint8_t  *button_history,uint8_t  curbtn){
+	//static uint8_t button_history = 0;
 	uint8_t pressed = 0;
-	button_history = button_history << 1;
-	button_history |= read_btn(curbtn);
-	if ((button_history & 0b11000111) == 0b00000111) {
+	*button_history = *button_history << 1;
+	*button_history |= read_btn(curbtn);
+	if ((*button_history & 0b11000111) == 0b00000111) {
 		pressed = 1;
-		button_history = 0b11111111;
+		*button_history = 0b11111111;
 	}
 	return pressed;
 }
@@ -110,19 +88,26 @@ int main() {
 }
 
 ISR (WDT_vect){
-
-	if (debounce(0x02)==1){
+/*
+	if (debounce(&mute_history,0x01)==1){
 		PORTB ^= _BV(PB1);//flip led 1
-	} else	if (debounce(0x04)==1){
+	}
+	if (debounce(&volm_history,0x04)==1){
 		PORTB ^= _BV(PB3);//flip led 2
+	}
+*/
+	if (debounce(&volp_history,0x02)==1){
+		PORTB ^= _BV(PB3);//flip led 1
 	}
 
 
+/*
 	for (int d=0x01;d<0x04;d<<1){
 		if (debounce(d)==1){
 			PORTB ^= _BV(PB1);//flip led 1
 		}
 	}
+*/
 
 	//PORTB ^= _BV(PB1);//flip led 1
 
