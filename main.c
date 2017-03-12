22814168e923b9b9faf7d42ea1622c0487faf1ef
antoine <mr-maurice@wanadoo.fr>
2017-03-12 16:02:29 +0100

    tranche de fuck

--- main.c
+++ main.c
@@ -1,26 +1,23 @@
-#include <avr/io.h>
+/* ATtiny85 Sony NEX/Alpha Remote Control
+
+   David Johnson-Davies - www.technoblogy.com - 12th April 2015
+   ATtiny85 @ 1 MHz (internal oscillator; BOD disabled)
+
+   CC BY 4.0
+   Licensed under a Creative Commons Attribution 4.0 International license:
+   http://creativecommons.org/licenses/by/4.0/
+ */
+
+#include <avr/sleep.h>
 #include <util/delay.h>
 #include <avr/interrupt.h>
-#include <avr/sleep.h>
-#define nop() __asm__ __volatile__ ("nop \n\t")
-/*
-uint8_t read_button_mute();
-uint8_t read_button_volp();
-uint8_t  read_button_volm();
- */
-uint8_t  read_btn(uint8_t);
-uint8_t debounce(uint8_t  *button_history,uint8_t);
-void SetupPCM();
-void Pulse (int carrier, int gap);
-void SendSony (unsigned long code);
-void Transmit (int address, int command) ;
 
-//0x01: mute,0x02:volp, 0x04:volm
-//volatile uint8_t curbtn=0x01;
-
-uint8_t mute_history=0;
-uint8_t volp_history=0;
-uint8_t volm_history=0;
+// Pin assignments
+const int IRout = 1;   // OC0B
+const int Shutter = 4;
+const int TwoSecs = 2;
+const int Video = 0;
+const int LED = 3;
 
 // Remote control
 const int Address = 0x1E3A;
@@ -28,77 +25,24 @@ const int ShutterCode = 0x2D;
 const int TwoSecsCode = 0x37;
 const int VideoCode = 0x48;
 
+// IR transmitter **********************************************
+
 const int top = 24;    // 1000000/25 = 40kHz
 const int match = 18;  // pulses with approx 25% mark/space ratio
 
-void setup() {
-	//LEDS
-	DDRB |= _BV(PB1);//led IR
-	//PORTB|= _BV(PB1);//led IR allumee
-
-	DDRB |= _BV(PB3);//led visible
-	PORTB &=~ _BV(PB3);//led visible allumee
-
-	sei();
-	//set_sleep_mode(SLEEP_MODE_PWR_DOWN);
-	// Disable ADC to save power
-	ADCSRA &= ~(1<<ADEN);
-	SetupPCM();
-	//SetupPinChange();
-	//watchdog configuration
-	WDTCR |= (1<<WDTIE);//generate interrupt after each time out
-}
-
-/**
- * sets up ports to read a specific button
- * returns 1 if the selected button is pressed
- */
-uint8_t read_btn(uint8_t  curbtn){
-	uint8_t ret=0x00;
-	if (curbtn==0x01){
-		DDRB &=~_BV(PB2);//PB2 en entree
-		PORTB |=_BV(PB2);//pull-up actif
-		nop();nop();nop();nop();
-		ret= ( (PINB & _BV(PB2)) == 0 );
-	} else if (curbtn==0x02){
-		DDRB &=~_BV(PB0);//PB0 en entree
-		PORTB |=_BV(PB0);//pull up sur PB0
-		DDRB |=_BV(PB2);//PB2 en sortie
-		PORTB &=~_BV(PB2);//PB2 a 0
-		nop();nop();nop();nop();
-		ret= ( (PINB & _BV(PB0)) == 0 );//lecture de PB0
-	} else if (curbtn==0x04){
-		DDRB &=~_BV(PB4);
-		PORTB |=_BV(PB4);
-		DDRB |=_BV(PB0);
-		PORTB &=~_BV(PB0);
-		nop();nop();nop();nop();
-		ret= ((PINB & (1<<PB4)) == 0);//lecture de PB4
-	}
-	return ret;
-}
-
-/***
- * curbtn: one of 0x01,0x02,0x04, matches mute, vol+,vol-
- * returns 1 if the button is considered pressed
- */
-uint8_t debounce(uint8_t  *button_history,uint8_t  curbtn){
-	uint8_t pressed = 0;
-	*button_history = *button_history << 1;
-	*button_history |= read_btn(curbtn);
-	if ((*button_history & 0b11000111) == 0b00000111) {
-		pressed=1;
-		*button_history = 0b11111111;
-	}
-	return pressed;
-}
-
+// Set up Timer/Counter0 to output PCM on OC0B
 void SetupPCM () {
 	TCCR0A = 3<<COM0B0 | 3<<WGM00; // Inverted output on OC0B and Fast PWM
 	TCCR0B = 1<<WGM02 | 1<<CS00;   // Fast PWM and divide by 1
 	OCR0A = top;                   // 40kHz
 	OCR0B = top;                   // Keep output low
+}
 
+// Set up Pin Change interrupts on button pins
+void SetupPinChange () {
+	PCMSK = 1<<Shutter | 1<<TwoSecs | 1<<Video;
+	// Enable interrupt
+	GIMSK = GIMSK | 1<<PCIE;
 }
 
 void Pulse (int carrier, int gap) {
@@ -124,38 +68,52 @@ void SendSony (unsigned long code) {
 	}
 }
 
+// Transmit code and light LED
 void Transmit (int address, int command) {
 	unsigned long code = (unsigned long) address<<7 | command;
-	PORTB|= _BV(PB3);
+	DDRB |= _BV(PB3);
 	SendSony(code);
 	_delay_ms(11);
 	SendSony(code);
-	PORTB&=~ _BV(PB3);
+	DDRB &=~ _BV(PB3);
+}
+
+// Interrupt **********************************************
+
+// Use PCINT0 just to wake up from sleep
+ISR(PCINT0_vect) {
+}
+
+// Setup **********************************************
+
+void setup() {
+	/*
+	pinMode(Shutter, INPUT_PULLUP);
+	pinMode(TwoSecs, INPUT_PULLUP);
+	pinMode(Video, INPUT_PULLUP);
+	 */
+	DDRB |= _BV(PB1);//led IR
+	DDRB |= _BV(PB3);//led visible
+	//set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+	// Disable ADC to save power
+	ADCSRA &= ~(1<<ADEN);
+	SetupPCM();
+	//SetupPinChange();
 }
 
 int main() {
 	setup();
 	while(1){
 		// Go to sleep
-		Transmit(Address, ShutterCode);
 		//sleep_enable();
 		//sleep_cpu();
+		// Come here after pin change interrupt wakes us from sleep
+		/*
+  if (!digitalRead(Shutter)) Transmit(Address, ShutterCode);
+  else if (!digitalRead(TwoSecs)) Transmit(Address, TwoSecsCode);
+  else if (!digitalRead(Video)) Transmit(Address, VideoCode);
+		 */
+		Transmit(Address, ShutterCode);
+		// Wait for all buttons to be released
 	}
-}
-
-ISR (WDT_vect){
-
-	if (debounce(&volp_history,0x02)==1){
-		PORTB ^= _BV(PB3);//flip led 2
-	}
-
-	if (debounce(&mute_history,0x01)==1){
-		PORTB &= ~_BV(PB1);//turn off
-		PORTB &= ~_BV(PB3);//turn off
-	}
-
-	if (debounce(&volm_history,0x04)==1){
-		PORTB ^= _BV(PB1);//flip led 2
-	}
-
 }
